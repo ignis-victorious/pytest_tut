164e4fa2598b1927f5b03495dd137b5a59bb1269
Emmanuele Levi <ignisvictorious@gmail.com>
2025-06-29 19:59:14 +0100

    Initial commit - Starting code

---
+++ main.py
@@ -0,0 +1,22 @@
+#  _______________
+#  Import LIBRARIES
+#  Import FILES
+#  _______________
+
+# def main():
+#     print("Hello from pytest-tut!")
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+def add_some_numbers(x: int, y: int) -> int:
+    """Well, add some numbers..."""
+    return x + y
+
+
+#  _______________
+#  Import LIBRARIES
+#  Import FILES
+#  _______________
---
+++ test.py
@@ -0,0 +1,13 @@
+#  _______________
+#  Import LIBRARIES
+#  Import FILES
+from main import add_some_numbers
+
+#  _______________
+
+
+print(add_some_numbers)
+
+
+def test_add_some_numbers() -> None:
+    assert True
