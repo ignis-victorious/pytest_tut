86f45949a23c9dccf1ba9981363713109cba1487
Emmanuele Levi <ignisvictorious@gmail.com>
2025-06-30 21:15:04 +0100

    All good but some points a little obscure

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 #  _______________
 #  Import LIBRARIES
+import requests as rq
+from requests import exceptions
+
 #  Import FILES
 #  _______________
 
 
-class Person:
-    def __init__(self, fname: str, lname: str, age: int) -> None:
-        self.fname: str = fname
-        self.lname: str = lname
-        self.age: int = age
-
-    @property
-    def full_name(self) -> str:
-        return f"{self.fname} {self.lname}"
-
-    def update_age(self, new_age: int) -> None:
-        if new_age > 0:
-            self.age = new_age
-            # self.age = new_age + 1  # To test the test error
-            return
-        raise ValueError
+"""Requests code."""
 
 
-john: Person = Person(fname="John", lname="Smith", age=55)
-print(vars(john))
-print(vars(john).keys())
-print(vars(john).values())
+def make_get_request(url: str) -> rq.Response | dict[str, str]:
+    try:
+        response: rq.Response = rq.get(url=url)
+    except exceptions.RequestException:
+        return {"Error": "oh no!"}
+    else:
+        return response.json()["headers"]
+
+
+works: rq.Response | dict[str, str] = make_get_request(url="https://httpbin.org/get")
+print(works, "\n\n")
+fails: rq.Response | dict[str, str] = make_get_request(url="https://notasite.abc")
+print(fails)
 
 
 # if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,73 +1,36 @@
 #  _______________
 #  Import LIBRARIES
-import pytest
+# import pytest
+from unittest.mock import Mock
+
+from pytest_mock import MockerFixture
+from requests import Response
+from requests.exceptions import RequestException
 
 #  Import FILES
-from main import Person
+from main import make_get_request
 
 #  _______________
 
-
-# def test_person_creation() -> None:
-#     test_instance: Person = Person(fname="John", lname="Smith", age=55)
-#     assert test_instance.fname == "John"  # "Johns" to force a failed test
-#     assert test_instance.lname == "Smith"
-#     assert test_instance.age == 55
+"""Tests for the get-request code."""
+# For mock we dont want to have to rely on dependencies, plus we can t
 
 
-def test_person_creation() -> None:
-    test_instance: Person = Person(fname="John", lname="Smith", age=55)
-    expected_key: list[str] = [
-        "fname",
-        "lname",
-        "age",
-    ]
-    expected_val: list[str | int] = [
-        "John",
-        "Smith",
-        55,
-    ]
+def test_valid_response(mocker: MockerFixture) -> None:
+    mock_response: Mock = mocker.Mock()
+    mock_response.json.return_value = {"headers": {"tst": 123}}
+    mocker.patch("main.rq.get", return_value=mock_response)
 
-    assert list(vars(test_instance).keys()) == expected_key
-    assert list(vars(test_instance).values()) == expected_val
+    result: Response | dict[str, str] = make_get_request(url="validurl")
+    assert result == {"tst": 123}
+    # assert result == {}
 
 
-# def test_person_creation() -> None:
-#     test_instance: Person = Person(fname="John", lname="Smith", age=55)
-#     expected: list[str] = ["fname", "lname", "age"]  # To fail: ["fname", "name", "age"]
+# You should import RequestException directly from requests.exceptions and use it in your test instead of accessing it as an attribute of the requests module.
+def test_invalid_response(mocker: MockerFixture) -> None:
+    mocker.patch("main.rq.get", side_effect=RequestException)
+    # mocker.patch("main.rq.get", side_effect=rq.exceptions.RequestException)
+    # mocker.patch("main.rq.get", side_effect=ValueError)
 
-#     assert list(vars(test_instance).keys()) == expected
-#     assert test_instance.fname == "John"
-#     assert test_instance.lname == "Smith"
-#     assert test_instance.age == 55
-
-
-def test_full_name() -> None:
-    test_instance_a: Person = Person(fname="John", lname="Smith", age=55)
-    assert test_instance_a.full_name == "John Smith", (
-        "Something is wrong with the full name property!"
-    )
-    test_instance_b: Person = Person(fname="James", lname="Smith", age=55)
-    assert test_instance_b.full_name == "James Smith", (
-        "Something is wrong with the full_name property!"
-    )
-    test_instance_c: Person = Person(fname="abc", lname="def", age=55)
-    assert test_instance_c.full_name == "abc def", (
-        "Something is wrong with the full_ name property!"
-    )
-
-
-def test_setting_age() -> None:
-    test_instance_a: Person = Person(fname="John", lname="Smith", age=55)
-    test_instance_a.update_age(new_age=100)
-    assert test_instance_a.age == 100, "No respect for a venerable age!"
-    # test_instance_b: Person = Person(fname="John", lname="Smith", age=55)
-    # test_instance_b.update_age(new_age=-1)
-    # assert test_instance_b.age == -1
-
-
-def test_setting_age_with_invalid_input() -> None:
-    test_instance: Person = Person(fname="John", lname="Smith", age=55)
-    with pytest.raises(expected_exception=ValueError):
-        # test_instance.update_age(new_age=-10)  # This will raise an error but we want to check the error is raised!
-        test_instance.update_age(new_age=-10)
+    result: Response | dict[str, str] = make_get_request(url="invalidurl")
+    assert result == {"Error": "oh no!"}
